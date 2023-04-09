a93b12c104f3ecb880aafa5b901739ee28116daa
cuong <nvc14122002@gmail.com>
2023-04-09 09:18:20 +0700

    Thêm file đầu tiên

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyCuaHangGiaDung
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new DangNhap());
+
+            //Application.Run(new View.HeThong());
+            //Application.Run(new View.QuanLyHoaDon());
+            //Application.Run(new View.QuanLyNhanVien());
+        }
+    }
+}
