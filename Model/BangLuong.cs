1b6bf2133f1906fa0bc9373b2282e1038ac21c49
hiephoan1 <hoangu2008@gmail.com>
2023-04-12 09:27:52 +0700

    Thêm Model

---
+++ Model/BangLuong.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyCuaHangGiaDung.Model
+{
+    public class BangLuong
+    {
+        private string _Thang;
+        private string _MaLuong;
+        private string _MaNV;
+        private string _TenNV;
+        private double _HeSoLuong;
+        private int _SoNgayLam;
+        private double _Thuong;
+        private double _Phat;
+        private double _PhuCap;
+        private double _ThucLinh;
+
+        public string Thang { get => _Thang; set => _Thang = value; }
+        public string MaLuong { get => _MaLuong; set => _MaLuong = value; }
+        public string MaNV { get => _MaNV; set => _MaNV = value; }
+        public string TenNV { get => _TenNV; set => _TenNV = value; }
+        public double HeSoLuong { get => _HeSoLuong; set => _HeSoLuong = value; }
+        public int SoNgayLam { get => _SoNgayLam; set => _SoNgayLam = value; }
+        public double Thuong { get => _Thuong; set => _Thuong = value; }
+        public double Phat { get => _Phat; set => _Phat = value; }
+        public double PhuCap { get => _PhuCap; set => _PhuCap = value; }
+        public double ThucLinh { get => _ThucLinh; set => _ThucLinh = value; }
+
+        public BangLuong () { }
+    }
+}
---
+++ Model/TK.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyCuaHangGiaDung.Model
+{
+    public class TK
+    {
+        private string _TaiKhoan;
+        private string _MatKhau;
+        private string _Quyen;
+
+        public string TaiKhoan { get => _TaiKhoan; set => _TaiKhoan = value; }
+        public string MatKhau { get => _MatKhau; set => _MatKhau = value; }
+        public string Quyen { get => _Quyen; set => _Quyen = value; }
+
+        public TK() { }
+    }
+}
